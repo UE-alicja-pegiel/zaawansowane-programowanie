1ae32e860f3e42e79d846f1d304ec726345b3fda
Student <alicja.pegiel@edu.uekat.pl>
2021-11-21 18:00:08 +0100

    zadanko 1

--- zad1.py
+++
@@ -1,8 +0,0 @@
-
-
-def hello_name(name: str, surname: str) -> str:
-    return f"Cześć {name} {surname}!"
-
-
-name = hello_name("Alicja", "Pęgiel")
-print(name)
--- zad2.py
+++
@@ -1,7 +0,0 @@
-
-
-def multi(number1: int, number2: int) -> int:
-    return number1 * number2
-
-
-print(multi(5, 4))
